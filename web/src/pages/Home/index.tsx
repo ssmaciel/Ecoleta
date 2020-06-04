e6e950effee274b76adf70d7ba6f9e7258b10309
Samuel Maciel <samuel.maciel@mag-it.com.br>
2020-06-03 22:50:02 -0300

    Implementação Front End Web

---
+++ web/src/pages/Home/index.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { FiLogIn } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
+
+import logo from '../../assets/logo.svg';
+import './styles.css';
+
+const Home = () => {
+  return (
+    <div id="page-home">
+      <div className="content">
+        <header>
+          <img src={logo} alt="Ecoleta" />
+        </header>
+        <main>
+          <h1>Seu marletplace de coleta de residulos</h1>´
+                    <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
+
+          <Link to="/create-point">
+            <span>
+              <FiLogIn />
+            </span>
+            <strong>Cadastre um ponto de coleta</strong>
+          </Link>
+        </main>
+      </div>
+
+    </div>
+  )
+}
+
+export default Home;
